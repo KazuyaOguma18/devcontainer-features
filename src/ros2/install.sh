83b2810159b1a9e35cbc643aa08fc4b20e1023ce
KazuyaOguma <kzy.basect@gmail.com>
2025-02-19 22:51:37 +0900

    add test.sh & minor distro

--- src/ros2/install.sh
+++ src/ros2/install.sh
@@ -11,17 +11,17 @@ echo "The ROS2 distro is: $DISTRO"
 export DEBIAN_FRONTEND=noninteractive
 
 # Set locale
-apt-get update && apt-get install -y locales
+apt-get update && apt-get install --yes --quiet --no-install-recommends locales
 locale-gen en_US en_US.UTF-8
 update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
 export LANG=en_US.UTF-8
 
 # Add the ROS2 apt-get repository
-apt-get install -y software-properties-common
+apt-get install --yes --quiet --no-install-recommends software-properties-common
 add-apt-repository universe
 
 # Add the ROS2 GPG key
-apt-get update && apt-get install -y curl
+apt-get update && apt-get install --yes --quiet --no-install-recommends curl
 curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
 
 # Add the repository to your sources list
@@ -29,8 +29,11 @@ echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-a
 
 # Install development tools and ROS tools
 apt-get update
-apt-get install -y ros-${DISTRO}-${PACKAGE} ros-dev-tools python3-argcomplete
+apt-get install --yes --quiet --no-install-recommends ros-${DISTRO}-${PACKAGE} ros-dev-tools python3-argcomplete
 
 echo "source /opt/ros/${DISTRO}/setup.bash" >> $_REMOTE_USER_HOME/.bashrc
+
+# We should clean up after ourselves
+rm -rf /var/lib/apt/lists/*
 
 export DEBIAN_FRONTEND=
